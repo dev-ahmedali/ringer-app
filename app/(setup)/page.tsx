f1732bcef82944041f7cd7cca9074c04a7bae879
dev-ahmedali <ahmedali69325@gmail.com>
2023-09-11 13:59:28 +0600

    setup prisma model

---
+++ app/(setup)/page.tsx
@@ -0,0 +1,26 @@
+import { db } from "@/lib/db";
+import { InitialProfile } from "@/lib/initial-profile";
+import { redirect } from "next/navigation";
+
+const SetupPage = async () => {
+    const profile = await InitialProfile();
+
+    const server = await db.server.findFirst({
+        where: {
+            members: {
+                some: {
+                    profileId: profile.id
+                }
+            }
+        }
+    });
+
+    if(server) {
+        return redirect(`/server/${server.id}`)
+    }
+    return ( 
+        <div>Create a server</div>
+     );
+}
+ 
+export default SetupPage;
