595e540cd37e3bc6442e5d7ca4f2977add7ef721
dev-ahmedali <ahmedali69325@gmail.com>
2023-09-19 00:36:06 +0600

    navigation sidebar

---
+++ components/navigation/navigation-item.tsx
@@ -0,0 +1,22 @@
+'use client'
+
+import { cn } from "@/lib/utils"
+
+
+interface NavigationItemsProps {
+    id: string;
+    imageUrl: string;
+    name: string
+}
+
+export const NavigationItem = ({
+    id,
+    imageUrl,
+    name
+}: NavigationItemsProps ) => {
+  return (
+    <div>
+        Server 
+    </div>
+  )
+}
---
+++ components/navigation/navigation-sidebar.tsx
@@ -0,0 +1,42 @@
+import { CurrentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+import { redirect } from "next/navigation";
+import { NavigationAction } from "./navigation-action";
+import { Separator } from "@/components/ui/separator";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { NavigationItem } from "@/components/navigation/navigation-item";
+
+export const NavigationSidebar = async () => {
+  const profile = await CurrentProfile();
+
+  if (!profile) {
+    return redirect("/");
+  }
+
+  const servers = await db.server.findMany({
+    where: {
+      members: {
+        some: {
+          profileId: profile.id,
+        },
+      },
+    },
+  });
+  return (
+    <div className="space-y-4 flex flex-col items-center h-full text-primary w-full dark:bg-[#1E1F22] py-3">
+      <NavigationAction />
+      <Separator className="h-[2px] bg-zinc-300 dark:bg-zinc-700 rounded-md mx-auto w-10 " />
+      <ScrollArea className="flex-1 w-full">
+        {servers.map((server) => (
+          <div key={server.id} className="mb-4">
+            <NavigationItem
+              id={server.id}
+              imageUrl={server.imageUrl}
+              name={server.name}
+            />
+          </div>
+        ))}
+      </ScrollArea>
+    </div>
+  );
+};
