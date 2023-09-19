4e60246616e3f9f2536fad0e565e5086f661877f
dev-ahmedali <ahmedali69325@gmail.com>
2023-09-19 20:25:50 +0600

    updated navigation sidebar

--- components/navigation/navigation-sidebar.tsx
+++ components/navigation/navigation-sidebar.tsx
@@ -1,10 +1,12 @@
 import { CurrentProfile } from "@/lib/current-profile";
 import { db } from "@/lib/db";
 import { redirect } from "next/navigation";
-import { NavigationAction } from "./navigation-action";
+import { NavigationAction } from "@/components/navigation/navigation-action";
 import { Separator } from "@/components/ui/separator";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { NavigationItem } from "@/components/navigation/navigation-item";
+import { ModeToggle } from "@/components/mode-toggle";
+import { UserButton } from "@clerk/nextjs";
 
 export const NavigationSidebar = async () => {
   const profile = await CurrentProfile();
@@ -37,6 +39,17 @@ export const NavigationSidebar = async () => {
           </div>
         ))}
       </ScrollArea>
+      <div className="pb-3 mt-auto flex items-certer flex-col gap-y-4">
+        <ModeToggle />
+        <UserButton 
+         afterSignOutUrl="/"
+         appearance={{
+          elements: {
+            avatarBox: "h-[48px] w-[48px]"
+          }
+         }}
+        />
+      </div>
     </div>
   );
 };
